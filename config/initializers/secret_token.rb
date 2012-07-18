57adb3fd388fc0352171ff4d87cd93ab5475c39f
hasegawa <info@the-ego.tv>
2012-07-18 12:12:45 +0900

    InitialCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialQuotes::Application.config.secret_token = '99d19d6e7657a584f7cdf8ca422f1564f6c2f15c424755dc53ef858a19da9a751a768446351c3f3044c4472e07f78247efa4a7ff018fa3ffc34e44d51dfff43d'
