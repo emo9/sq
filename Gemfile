f3606f8f2bb513f9e83596b5f6c275b0e808e1cc
hasegawa <info@the-ego.tv>
2012-07-19 17:40:32 +0900

    Implemented Pagination with kaminari

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development, :test do
 end
 
 gem 'less-rails-bootstrap'
+gem 'kaminari'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -17,4 +17,7 @@ class Quote < ActiveRecord::Base
   validates :reference, :presence => true,
                         :length => { :maximum => 100 }
   validates :remark,    :length => { :maximum => 500 }
+  
+  paginates_per 5
+  default_scope :order => 'created_at DESC'
 end
